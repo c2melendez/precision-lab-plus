622fd17ca79b60ba01db1e0eda57770585111657
c2melendez <c2.melendez@gmail.com>
2026-09-25 01:47:05 -0600

    S26.3: alinear subiconos con familia visual aprobada

--- frontend/src/components/ModeIcon.tsx
+++ frontend/src/components/ModeIcon.tsx
@@ -20,13 +20,13 @@ export function ModeIcon({ name, ...props }: { name: ModeIconName } & SVGProps<S
     return <svg {...common} {...props}><rect x="4" y="4" width="16" height="16" rx="2"/><path d="M9.33 4v16M14.66 4v16M4 9.33h16M4 14.66h16"/></svg>;
   }
   if (name === "graph") {
-    return <svg {...common} {...props}><path d="M4 19V5M4 19h16"/><path d="m6 15 4-5 3 3 5-7"/></svg>;
+    return <svg {...common} {...props}><path d="M4 20V10M10 20V6M16 20V3M22 20V13"/><path d="M2 20h22"/></svg>;
   }
   if (name === "statistics") {
-    return <svg {...common} {...props}><path d="M5 19v-5M10 19V9M15 19V5M20 19v-8"/><path d="M3 19h18"/></svg>;
+    return <svg {...common} {...props}><path d="M17.5 4H6.5l6 8-6 8h11"/></svg>;
   }
   if (name === "geometry") {
-    return <svg {...common} {...props}><path d="m12 3 8 5v8l-8 5-8-5V8l8-5Z"/><path d="m4 8 8 5 8-5M12 13v8"/></svg>;
+    return <svg {...common} {...props}><path d="M4 19 12 5l8 14H4Z"/><circle cx="12" cy="13" r="3.2"/></svg>;
   }
   return <svg {...common} {...props}><path d="M5 18 18 5l2 2L7 20H5v-2Z"/><path d="m12 9 3 3M9 12l3 3M15 6l3 3"/></svg>;
 }
